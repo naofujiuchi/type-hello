d426b77a91dfd46e32df4b0713022d117078ac70
naofujiuchi <naofujiuchi@gmail.com>
2013-10-15 17:09:26 +0900

    keep memory space (malloc)

--- type-hello.c
+++ type-hello.c
@@ -1,51 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_LEN 100
 
 //create type 'CipherText'
 typedef struct{
   int shift;
-  char text[MAX_LEN];
+  char* text;
 }CipherText;
 
 //declare variant
 int i;
 
 //declare function
-void cipher(const char* plain_text, int shift, CipherText* cipher);
-void decipher(const CipherText* cipher, char* plain_text);
+static int stringlen(const char* text);
+void initCipherText(CipherText* cipher);
+void deleteCipherText(CipherText* cipher);
+int cipher(const char* plain_text, int shift, CipherText* cipher);
+char* decipher(const CipherText* cipher);
+
+//measure input string length
+static int stringlen(const char* text){
+  int max_length = 0;
+  while(text[max_length] != 0){
+    max_length++;
+  }
+  return max_length;
+}
+
+//initialize CipherText object and keep memory space
+void initCipherText(CipherText* cipher){
+  cipher->shift = 0;
+  cipher->text = NULL;
+}
+
+//delete CipherText object and free memory space
+void deleteCipherText(CipherText* cipher){
+  if(cipher->text != NULL){
+    free(cipher->text);
+    cipher->text = NULL;
+  }
+}
 
 //coding function 'cipher'
 //input... ("HELLO WORLD", 3, &a)
 //output... &a->text: KHOOR#ZRUOG
-void cipher(const char* plain_text, int shift, CipherText* ioCipher){
+int cipher(const char* plain_text, int shift, CipherText* ioCipher){
   ioCipher->shift = shift;
-  for(i = 0; i < MAX_LEN; i++){
-    if(plain_text[i] == 0){
-      ioCipher->text[i] = 0;
-      break;
-    }
+  int length = stringlen(plain_text);
+  if(ioCipher->text != NULL){
+    free(ioCipher->text);
+  }
+  ioCipher->text = malloc(sizeof(char)*(length+1));
+  if(ioCipher->text == NULL){
+    return 0;
+  }
+  for(i = 0; i < length; i++){
     ioCipher->text[i] = plain_text[i] + ioCipher->shift;
   }
+  ioCipher->text[length] = 0;
+  return 1;
 }
 
 //decode function 'decipher'
 //input... (&a, plain_text)
 //output... plain_text: HELLO WORLD
-void decipher(const CipherText* cipher, char* plain_text){
-  for(i = 0; i < MAX_LEN; i++){
-    if(cipher->text[i] == 0){
-      plain_text[i] = 0;
-      break;
-    }
-    plain_text[i] = cipher->text[i] - cipher->shift;
+char* decipher(const CipherText* cipher){
+  int length = stringlen(cipher->text);
+  char* text = malloc(sizeof(char)*(length + 1));
+  if(text == NULL){
+    return NULL;
   }
+  for(i = 0; i < length; i++){
+    text[i] = cipher->text[i] - cipher->shift;
+  }
+  text[length] = 0;
+  return text;
 }
 
 int main(int argc, const char* argv[]){
   CipherText a;
-  char plain[MAX_LEN];
-  cipher("Hello world!", 3, &a);
-  decipher(&a, plain);
-  printf("%s = %s\n", plain, a.text);
+  char* plain;
+  initCipherText(&a);
+  if(cipher("Hello world!", 3, &a) == 1){
+    plain = decipher(&a);
+  }
+  if(plain != NULL){
+    printf("%s = %s\n", plain, a.text);
+    free(plain);
+  }
+  deleteCipherText(&a);
   return 0;
 }
